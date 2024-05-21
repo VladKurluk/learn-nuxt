41f6c741e1bea9c66bdc029ec781bff4ecc7b42c
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-05-21 14:56:55 +0300

    Server side error handling

--- error.vue
+++ error.vue
@@ -4,6 +4,13 @@
             <template v-if="error.statusCode === 404">
                 <h1>404</h1>
                 <p><strong>Sorry, that page doesn't exist.</strong></p>
+                <strong>{{ error.message }}</strong>
+                <button
+                    @click="handleError('/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3')"
+                    class="border mt-4 py-2.5 px-2 rounded-md bg-green-300 border-green-700"
+                >
+                    Go to the first lesson
+                </button>
             </template>
             <template v-else>
                 <h2>{{ error.statusCode }}</h2>
@@ -11,7 +18,7 @@
                 <strong>{{ error.message }}</strong>
                 </p>
                 <button
-                    @click="handleError"
+                    @click="handleError('/')"
                     class="border mt-4 py-2.5 px-2 rounded-md bg-green-300 border-green-700"
                 >
                     Clear errors
@@ -31,6 +38,6 @@ const props = defineProps({
   error: Object as () => NuxtError
 });
 
-const handleError = () => clearError({ redirect: '/' });
+const handleError = (redirectTo) => clearError({ redirect: redirectTo });
 </script>
 
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -33,6 +33,31 @@
   const route = useRoute();
   const course = useCourse();
 
+  definePageMeta({
+    validate({ params }) {
+      const course = useCourse();
+      const chapter = course.chapters.find(
+          (chapter) => chapter.slug === params.chapterSlug
+      );
+      if (!chapter) {
+        return createError({
+          statusCode: 404,
+          message: 'Chapter not found',
+        });
+      }
+      const lesson = chapter.lessons.find(
+          (lesson) => lesson.slug === params.lessonSlug
+      );
+      if (!lesson) {
+        return createError({
+          statusCode: 404,
+          message: 'Lesson not found',
+        });
+      }
+      return true;
+    },
+  });
+
   const chapter = computed(() => {
     return course.chapters.find((chapter) => {
       return chapter.slug === route.params.chapterSlug
